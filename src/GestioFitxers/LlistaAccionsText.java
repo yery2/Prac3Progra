5c39f14627840ac0b03f81976793f702c74fc68b
Marc Badia <marcbadiamarti@gmail.com>
2025-01-07 17:47:12 +0100

    1747

--- src/GestioFitxers/LlistaAccionsText.java
+++ src/GestioFitxers/LlistaAccionsText.java
@@ -7,8 +7,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-import Classes.Accio;
 import Llistes.LlistaAssociacions;
+import Classes.*;
 
 /**
  * @author Yeray Zarza
@@ -82,7 +82,7 @@ public class LlistaAccionsText {
     public int getNElem() {
         return nElem;
     }
-
+/* 
     public void carregarAccions() {
         try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))) {
             String line;
@@ -103,6 +103,55 @@ public class LlistaAccionsText {
         } catch (NumberFormatException e) {
             System.out.println("Error en el format de dades numèriques: " + e.getMessage());
         }
+    }*/
+    public void carregarAccions() {
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                StringTokenizer st = new StringTokenizer(line, ";");
+    
+                String codi = st.nextToken();
+                String titol = st.nextToken();
+                String responsable = st.nextToken();
+                String[] dateParts = st.nextToken().split("-");
+                int[] data = new int[3];
+                for (int i = 0; i < dateParts.length; i++) {
+                data[i] = Integer.parseInt(dateParts[i]);
+                }
+                String nomAssociacions = st.nextToken();
+                int cost = Integer.parseInt(st.nextToken());
+                short valoracions = Short.parseShort(st.nextToken());
+                int vegades = Integer.parseInt(st.nextToken());
+                boolean valida = Boolean.parseBoolean(st.nextToken());
+                boolean esXerrada = Boolean.parseBoolean(st.nextToken());
+                
+    
+                String[] associacions = nomAssociacions.split(",");
+                LlistaAssociacions llistaAs = new LlistaAssociacions();
+                for (String assoc : associacions) {
+                      
+                }
+                
+
+                
+                Accio ac;
+                if (esXerrada) {
+                    Data dataXerrada = new Data();
+                    dataXerrada.setData(data[2], data[1], data[0]);
+                    ac = new Xerrada(titol, responsable, dataXerrada, vegades, valoracions, llistaAs);
+                } else {
+                    Data dataDisseny = new Data();
+                    dataDisseny.setData(data[2], data[1], data[0]);
+                    ac = new Demostracio(titol, responsable, dataDisseny, vegades, valida, valoracions, cost, llistaAs);
+                }
+
+                afegirAccio(ac);
+            }
+        } catch (IOException e) {
+            System.out.println("No es pot carregar el fitxer: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("Error en el format de dades numèriques: " + e.getMessage());
+        }
     }
 
     public void guardarAccions() {
@@ -116,6 +165,45 @@ public class LlistaAccionsText {
             System.out.println("No es pot guardar el fitxer: " + e.getMessage());
         }
     }
+
+    public Associacio getAssocPosicio(int pos) {
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))) {
+            String line;
+            int currentIndex = 0; 
+            while ((line = br.readLine()) != null) {
+                if (currentIndex == pos) { 
+                    String[] data = line.split(";");
+    
+                    
+                    if (data.length < 9) {
+                        System.out.println("Format incorrecte a la línia: " + line);
+                        return null;
+                    }
+    
+                    String[] membres = data[2].split(",");
+                    String[] titulacions = data[3].split(",");
+                    return new Associacio(
+                            data[0],  // ID
+                            data[1],  // Nom
+                            membres,  // Membres
+                            titulacions,  // Titulacions
+                            data[4],  // Algún campo adicional
+                            data[5],  // Algún campo adicional
+                            data[6],  // Algún campo adicional
+                            Integer.parseInt(data[7]), // Algún campo numérico
+                            Integer.parseInt(data[8])  // Algún campo numérico
+                    );
+                }
+                currentIndex++; // Incrementar el índice actual
+            }
+        } catch (IOException e) {
+            System.out.println("No es pot carregar el fitxer: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("Error en el format numèric: " + e.getMessage());
+        }
+        return null; // Retornar null si no se encuentra la posición
+    }
+    
 
 
 }
